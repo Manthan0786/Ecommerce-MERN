d38e77fcbe9000fa3835d09d9eef33cc44fd72ab
Manthan Bhardwaj <95605134+Manthan0786@users.noreply.github.com>
2023-03-25 20:33:20 -0600

    Retakes

--- frontend/ecommerce/src/productList.js
+++ frontend/ecommerce/src/productList.js
@@ -9,15 +9,18 @@ function Product() {
     const navigate = useNavigate();
     const [products, setProducts] = useState([]);
     async function fetchdata() {
-        const res = await axios.get('/products/');
+        const res = await axios.get('http://localhost:8080/products/');
         setProducts(res.data);
+        console.log(products);
     }
     useEffect(() => {
         fetchdata();
+        
+        console.log(typeof(products));
     }, [])
 
     const handleDelete = async (id) => {
-        const res = await axios.delete(`/products/${id}`)
+        const res = await axios.delete(`http://localhost:8080/products/${id}`)
         if (res.data._id) {
             setProducts(products.filter(p => p._id !== res.data._id))
         }
@@ -33,20 +36,20 @@ function Product() {
             italic text-2xl font-semibold text-amber'>Products</div>
             <div className='grid grid-cols-3 px-4 mt-4 gap-4 mb-4'>
                 {
-                    products.map(p => {
+                   products && products.map(p => {
                         return (
                             <div className='w-auto bg-white rounded-md shadow-md overflow-hidden basis-1/4
                        transform hover:scale-105 transition duration-300 static' key={p._id} >
                                 <img src={p.thumbnail} alt='product pic' className='w-full h-48 object-fit' />
-                                <div class="px-4 py-2">
+                                <div className="px-4 py-2">
                                     <div className='flex justify-between'>
-                                        <h2 class="text-gray-800 text-lg font-bold mb-2">{p.title}</h2>
-                                        <span class='new-price'>${p.price}</span>
+                                        <h2 className="text-gray-800 text-lg font-bold mb-2">{p.title}</h2>
+                                        <span className='new-price'>${p.price}</span>
                                     </div>
-                                    <p class="text-gray-600 text-sm leading-snug">{p.description}</p>
+                                    <p className="text-gray-600 text-sm leading-snug">{p.description}</p>
                                 </div>
-                                <div class="px-4 py-2 bg-gray-100 ">
-                                    <span class="rating-number">{p.rating}</span>
+                                <div className="px-4 py-2 bg-gray-100 ">
+                                    <span className="rating-number">{p.rating}</span>
                                 </div>
                                 <button className='bg-cyan-500 hover:bg-cyan-600 rounded-full absolute bottom-0 right-0' onClick={() => handleDelete(p._id)}>
                                     <DeleteIcon></DeleteIcon>
