c3aa46a72005b67075a62b05374c91995bf3c346
Manthan Bhardwaj <95605134+Manthan0786@users.noreply.github.com>
2023-04-26 21:32:36 -0600

    new changes

--- frontend/ecommerce/src/header.js
+++ frontend/ecommerce/src/header.js
@@ -7,7 +7,7 @@ function Header() {
     <>
       <header className="bg-black z-10 text-amber sticky top-0 left-0 right-0 flex justify-center items-center w-full h-16 mb-4">
         <Link to="/" >
-          <h1 className='text-white font-bold text-4xl hover:text-white hover:no-underline'>Ecommerce</h1>
+          <h1 className='text-white font-bold text-4xl hover:text-white hover:no-underline ml-4'>Ecommerce</h1>
         </Link>
         <NavBar></NavBar>
       </header>
